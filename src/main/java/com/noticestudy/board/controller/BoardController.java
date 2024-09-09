5d26eec19d214add0296448859d28d6afeb88e84
shkim00216 <shkim00216@gmail.com>
2024-09-09 23:31:44 +0900

    0909 2331

---
+++ src/main/java/com/noticestudy/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.noticestudy.board.controller;
+
+import com.noticestudy.board.dto.BoardDTO;
+import com.noticestudy.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor //생성자 어노테이션
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        return "index";
+    }
+}
---
+++ src/main/java/com/noticestudy/board/service/BoardService.java
@@ -0,0 +1,11 @@
+package com.noticestudy.board.service;
+
+import com.noticestudy.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+}
